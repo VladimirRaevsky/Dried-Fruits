f32ae095d920d7f7665dc4db6b97a293fa12e3c9
Vladimir R <medvedev-geny81@mail.ru>
2024-01-29 19:11:44 +0300

    jest has been added to the project

--- src/pages/CatalogPage/ui/CatalogPage.tsx
+++ src/pages/CatalogPage/ui/CatalogPage.tsx
@@ -1,3 +1,4 @@
+import { t } from 'i18next'
 import { type FC } from 'react'
 import cls from './CatalogPage.module.scss'
 
@@ -8,7 +9,7 @@ interface CatalogPageProps {
 const CatalogPage: FC<CatalogPageProps> = (props) => {
     const { className } = props
 
-    return <div className="">CatalogPage</div>
+    return <div className="">{t('CatalogPage')}</div>
 }
 
 export default CatalogPage
